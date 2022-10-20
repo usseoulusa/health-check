ec5ea37e4f49690ea3621ea4505fc0609a58e499
usseoulusa <usseoulusa@gmail.com>
2022-10-19 21:05:28 -0400

    Add initial version

---
+++ cpu_usage.py
@@ -0,0 +1,14 @@
+'''
+#!/usb/bin/env python3
+'''
+import psutil
+
+
+def check_cpu_usage(percent):
+    usage = psutil.cpu_percent()
+    return usage < percent
+
+if not check_cpu_usage(75):
+    print("ERROR! CPU is overloaded")
+else:
+    print("Everything ok")
